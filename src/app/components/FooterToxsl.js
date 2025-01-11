e83fd0df0fde7de68f11c529b26d061832291026
aadarsh360 <aadarshpandey64@gmail.com>
2025-01-11 12:40:59 +0530

    toxsl footer

---
+++ src/app/components/FooterToxsl.js
@@ -0,0 +1,137 @@
+import React from 'react'
+
+function FooterToxsl() {
+    return (
+        <footer className="background-dark-blue text-white py-5">
+
+            <div className="container">
+                <div className="row">
+                    {/* Company Column */}
+                    <div className="col-lg-3 col-md-6 col-12 mb-4">
+                        <h4>Company</h4>
+                        <ul className="list-unstyled">
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">About ToXSL</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Life At ToXSL</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">How We Work</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">What Our Clients Say</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Contact Us</a></li>
+                        </ul>
+                        <button className="btn btn-outline-light mt-3 mb-2">Schedule a Meeting</button>
+                    </div>
+
+                    {/* Services Column */}
+                    <div className="col-lg-3 col-md-6 col-12 mb-4">
+                        <h4>Services</h4>
+                        <ul className="list-unstyled">
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Mobile App Development Services</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Web Development Services</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">ERP Software Services</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Web Design Services</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Digital Marketing Services</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Artificial Intelligence</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Automated Testing</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">IoT Application Services</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">All Services</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Industries Column */}
+                    <div className="col-lg-2 col-md-6 col-12 mb-4">
+                        <h4>Industries</h4>
+                        <ul className="list-unstyled">
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Taxi Booking</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Lifestyle</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Healthcare</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Grocery Delivery</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Food Delivery</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Internet of Things</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Education</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Real Estate</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">E-Commerce</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Resources Column */}
+                    <div className="col-lg-2 col-md-6 col-12 mb-4">
+                        <h4>Resources</h4>
+                        <ul className="list-unstyled">
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Blog</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Hire Us</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Trainings</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Privacy Policy</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Terms & Conditions</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Company Australia</a></li>
+                            <li className='py-1 list-item'><a href="#" className="text-white text-decoration-none hover-pink link-hover">Company UK</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Contact Us Column */}
+                    <div className="col-lg-2 col-md-6 col-12">
+                        <h4>Contact Us</h4>
+                        <p className="mb-1 py-1 list-item hover-pink link-hover"><i className="bi bi-envelope"></i> us@toxsl.com</p>
+                        <p className="mb-1 py-1 list-item hover-pink link-hover"><i className="bi bi-telephone"></i> +1 205 922 7788,</p>
+                        <p className="mb-1 py-1 list-item hover-pink link-hover">+1 234 400 7788</p>
+                        <p className='mb-1 py-1 list-item'><i className="bi bi-geo-alt"></i> 30 N Gould St, STE R, Sheridan, WY 82801</p>
+
+                    </div>
+                </div>
+
+                <hr className='border-light' />
+                <div className='row'>
+                    <div className='col-lg-7 col-12'>
+                        <p className='fs-5'>Awards and Certifications</p>
+                    </div>
+
+                    <div className="col-lg-5 col-12 d-flex flex-wrap justify-content-around">
+                        <img
+                            src="https://toxsl.com/themes/new/img/iso1.svg"
+                            alt="Image 1"
+                            className="img-fluid"
+                            style={{ width: "100px", height: "50px" }}
+                        />
+                        <img
+                            src="https://toxsl.com/themes/new/img/iso2.svg"
+                            alt="Image 2"
+                            className="img-fluid"
+                            style={{ width: "100px", height: "50px" }}
+                        />
+                        <img
+                            src="https://toxsl.com/themes/new/img/yiiframework.svg"
+                            alt="Image 3"
+                            className="img-fluid"
+                            style={{ width: "100px", height: "50px" }}
+                        />
+                        <img
+                            src="https://toxsl.com/themes/new/img/naascom.svg"
+                            alt="Image 4"
+                            className="img-fluid"
+                            style={{ width: "100px", height: "50px" }}
+                        />
+                    </div>
+
+                </div>
+                <hr className="border-light" />
+
+
+                <div className="d-flex flex-wrap align-items-center">
+                    <div>
+                        <p className="mb-0">&copy; 2012 - 2025 <a href="#" className=" text-decoration-none">ToXSL Technologies Pvt Ltd.</a>
+                            All rights reserved. Hosted By<br></br>
+                            <a href="#" className=" text-decoration-none">jjWebHosting.com</a>
+                        </p>
+                    </div>
+
+                    <div className="mt-3">
+                        <a href="#" className="text-white me-3"><i className="bi bi-facebook"></i></a>
+                        <a href="#" className="text-white me-3"><i className="bi bi-twitter"></i></a>
+                        <a href="#" className="text-white me-3"><i className="bi bi-linkedin"></i></a>
+                        <a href="#" className="text-white"><i className="bi bi-instagram"></i></a>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default FooterToxsl
